76439f0cf96861e6a90a609c2ed4eaf51364d1c6
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-21 20:53:55 +0300

    Add initial files

---
+++ monty.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+  * main - monty bytecodes interpreter
+  * @argc: number of arguments
+  * @argv: array of arguments passed
+  * Return: EXIT_SUCCESS on success or EXIT_FAILURE on failure
+  */
+int main(int argc, char **argv)
+{
+	/* TODO: Monty Interpreter */
+}
