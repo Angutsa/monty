8c0055a63027701320b82bc37cf1067d63ced597
Zacharia Njue <njuezachary@gmail.com>
2023-03-22 20:03:33 +0300

    separated the files, pop.c. push.c and pall.c and edited monty.h

--- monty.h
+++ monty.h
@@ -2,19 +2,41 @@
 #define MONTY_H
 #include <stdio.h>
 #include <stdlib.h>
-
-/* Data structure for the stack */
-typedef struct stack_s {
-    int n;
-    struct stack_s *next;
-} stack_t;
-
-/* Global variable for the stack */
-extern stack_t *stack;
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct bus_s
+{
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
+}  bus_t;
+extern bus_t bus;
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
 
 /* Function prototypes */
-void push(char *arg);
-void pall(void);
+void f_push(stack_t **head, unsigned int number);
+void f_pall(stack_t **head, unsigned int number);
+void f_pop(stack_t **head, unsigned int counter);
 
 #endif /* MONTY_H */
 
